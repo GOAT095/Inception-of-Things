db5c1d0844eb00a4ec54136666c007957627d8cb
anassif <ubuntu@ubuntu2004.linuxvmimages.local>
2024-03-14 12:33:15 -0400

    server without flannerl nor token

---
+++ p1/master.sh
@@ -0,0 +1,2 @@
+#registering a server without flannel and with a token
+curl -sfL https://get.k3s.io | sh -s
--- p1/master/master.sh
+++
@@ -1,5 +0,0 @@
-#registering a server without flannel and with a token
-if curl -sfL https://get.k3s.io | sh -s - --flannel-backend none --token 12345;then
-    echo -e "instalattion was successful"
-else
-    echo -e "error k3s install"
