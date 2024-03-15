b93afa41dd9ac0b66cc6ab138fe85948199f567d
anassif <ubuntu@ubuntu2004.linuxvmimages.local>
2024-03-14 23:30:05 -0400

    starting k3s-agent hanging

--- p1/master/master.sh
+++ p1/master/master.sh
@@ -1,4 +1,4 @@
 #registering a server without flannel and with a token
 #644 to resolve rancher error https://thriveread.com/unable-to-read-etc-rancher-k3s-k3s-yaml-error/
-#kubeconfig_mode for rancher error k3s url is to define the agent
+#kubeconfig_mode for rancher error k3s & controle mode -  url is to define the agent
 curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=123456 sh -s
