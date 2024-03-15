c6583f64425f168577d00d6126d0231e7f7912f7
anassif <ubuntu@ubuntu2004.linuxvmimages.local>
2024-03-15 16:39:23 -0400

    need to check why its not working as agent

--- p1/worker/worker.sh
+++ p1/worker/worker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # Install k3s agent node in agent mode
-curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.111:6443 K3S_TOKEN=123456 sh -
+#curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.111:6443 K3S_TOKEN=123456 sh -
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent --server https://192.168.56.110:6443 --token mypassword" sh -s -
