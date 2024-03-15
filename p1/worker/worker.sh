f5c94f56a57f863688e8f5df30cd8cb267df18d5
anassif <ubuntu@ubuntu2004.linuxvmimages.local>
2024-03-14 23:55:21 -0400

    server without flannerl nor token

--- p1/worker/worker.sh
+++ p1/worker/worker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Install k3s agent node in agent mode
-curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=123456 sh -
+curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.111:6443 K3S_TOKEN=123456 sh -
