7d2972f4024abeb99b2238bb2b631191e82986ba
anassif <ubuntu@ubuntu2004.linuxvmimages.local>
2024-03-14 21:34:21 -0400

    server without flannerl nor token

--- p1/master.sh
+++ p1/master.sh
@@ -1,2 +1,3 @@
 #registering a server without flannel and with a token
-curl -sfL https://get.k3s.io | sh -s
+#644 to resolve rancher error https://thriveread.com/unable-to-read-etc-rancher-k3s-k3s-yaml-error/
+curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 644
