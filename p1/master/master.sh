4ba0685df520f8655442f569bdfd3664439698f9
anassif <ubuntu@ubuntu2004.linuxvmimages.local>
2024-03-16 00:07:18 -0400

    doesnt hang need to copy token from master for it to work

--- p1/master/master.sh
+++ p1/master/master.sh
@@ -1,4 +1,4 @@
 #registering a server without flannel and with a token
 #644 to resolve rancher error https://thriveread.com/unable-to-read-etc-rancher-k3s-k3s-yaml-error/
 #kubeconfig_mode for rancher error k3s & controller mode -  url is to define the agent
-curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 644 --flannel-backend none --token 12345
+curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 644 --prefer-bundled-bin --flannel-backend none --token 12345
