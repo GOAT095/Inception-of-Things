1fe15ec75e2e9c6501cec6e31129473c76bcf516
anassif <ubuntu@ubuntu2004.linuxvmimages.local>
2024-03-17 11:01:16 -0400

    slave hangs anassifSW: [INFO]  systemd: Starting k3s-agent

--- p1/master/master.sh
+++ p1/master/master.sh
@@ -1,7 +1,17 @@
-#registering a server without flannel and with a token
-#644 to resolve rancher error https://thriveread.com/unable-to-read-etc-rancher-k3s-k3s-yaml-error/
-#kubeconfig_mode for rancher error k3s & controller mode
-curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 644 --tls-san anassifS
+#old master
+#curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 644 --tls-san anassifS
+
+
+
+masterIP="192.168.56.110"
+k3sTokenFile="/var/lib/rancher/k3s/server/node-token"
+flags="--tls-san $masterIP --node-external-ip $masterIP"
+
+echo "[INFO] Install k3s on master-node"
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="$flags" K3S_NODE_NAME="anassifS" K3S_KUBECONFIG_MODE="644" sh -
+
+# echo "[INFO] Expose K3S_TOKEN from master-node for worker-node setup"
+# cp "$k3sTokenFile" /vagrant/resources/.generated/k3s.token
 
 #cp the vagrant token to vagrant folder
 sudo cat /var/lib/rancher/k3s/server/token >> /vagrant/token.env
