11685ac4f8df2d13fc88837ab64140fc48061b4a
GOAT095 <hakimoxxx@gmail.com>
2024-03-17 13:54:48 +0000

    anassifSW: Job for k3s-agent.service failed because the control process exited with error code

--- p1/worker/worker.sh
+++ p1/worker/worker.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # masterIP="192.168.56.110"
 # flags="--tls-san $masterIP --node-external-ip $masterIP"
-# Install k3s agent node in agent mode
-curl -sfL https://get.k3s.io | agent --server https://192.168.56.110:6443 --token-file /vagrant/token.env --node-ip=192.168.56.111  sh -
+
+k3s_Token=$(cat /vagrant/token.env)
+curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=$k3s_Token sh -
+
 # curl -sfL https://get.k3s.io | sh -s - 
 #curl -sfL https://get.k3s.io | K3S_URL=https://<manager ip address>:6443 K3S_TOKEN=K1073b3f02527159c80edf837727ae35cde5169707a7be0ecc91ca5a852e1e2c76f::server:c86cf36c0577ece1ef6d2cf072ece1c4 sh -
