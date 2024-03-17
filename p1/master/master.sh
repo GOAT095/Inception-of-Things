55cdd82093706a3ce9d5b65c9fa8f284e7399e1a
anassif <ubuntu@ubuntu2004.linuxvmimages.local>
2024-03-17 10:47:51 -0400

    anassifS Ready now

--- p1/master/master.sh
+++ p1/master/master.sh
@@ -1,7 +1,7 @@
 #registering a server without flannel and with a token
 #644 to resolve rancher error https://thriveread.com/unable-to-read-etc-rancher-k3s-k3s-yaml-error/
-#kubeconfig_mode for rancher error k3s & controller mode -  url is to define the agent
-curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 644 --tls-san anassifS --node-ip 192.168.56.110
+#kubeconfig_mode for rancher error k3s & controller mode
+curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 644 --tls-san anassifS
 
 #cp the vagrant token to vagrant folder
 sudo cat /var/lib/rancher/k3s/server/token >> /vagrant/token.env
--- p1/worker/worker.sh
+++ p1/worker/worker.sh
@@ -3,7 +3,7 @@
 # flags="--tls-san $masterIP --node-external-ip $masterIP"
 
 k3s_Token=$(cat /vagrant/token.env)
-curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=$k3s_Token sh -
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent --server https://192.168.56.110:6443 --token ${k3s-Token}" sh -s -
+#curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=$k3s_Token sh - --node-ip https://192.168.56.110
 
 # curl -sfL https://get.k3s.io | sh -s - 
-#curl -sfL https://get.k3s.io | K3S_URL=https://<manager ip address>:6443 K3S_TOKEN=K1073b3f02527159c80edf837727ae35cde5169707a7be0ecc91ca5a852e1e2c76f::server:c86cf36c0577ece1ef6d2cf072ece1c4 sh -
