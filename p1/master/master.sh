1e34a343a9496084239e7cb49e6da4a0ccdab536
anassif <ubuntu@ubuntu2004.linuxvmimages.local>
2024-03-17 23:14:56 -0400

    both master and slave ready seems to work fine

--- p1/master/master.sh
+++ p1/master/master.sh
@@ -4,10 +4,10 @@
 
 masterIP="192.168.56.110"
 k3sTokenFile="/var/lib/rancher/k3s/server/node-token"
-flags="--tls-san $masterIP --node-external-ip $masterIP"
+flags="--tls-san $masterIP --node-external-ip $masterIP"  #helps so k3s master listens on its real IP and accepts requests by putting its IP inside the cert.
 
 echo "[INFO] Install k3s on master-node"
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="$flags" K3S_NODE_NAME="anassifS" K3S_KUBECONFIG_MODE="644" sh -
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="$flags" K3S_NODE_NAME="anassifS" K3S_KUBECONFIG_MODE="644" sh - #K3S_KUBECONFIG_MODE for perm so its access rancher folder
 
 # echo "[INFO] Expose K3S_TOKEN from master-node for worker-node setup"
 # cp "$k3sTokenFile" /vagrant/resources/.generated/k3s.token
--- p1/worker/worker.sh
+++ p1/worker/worker.sh
@@ -3,7 +3,8 @@
 # flags="--tls-san $masterIP --node-external-ip $masterIP"
 
 k3s_Token=$(cat /vagrant/token.env)
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent --server https://192.168.56.110:6443 --token ${k3s-Token} --node-ip =https://192.168.56.110" sh -
+curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=${k3s_Token} sh -
+# curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent --server https://192.168.56.110:6443 --token ${k3s-Token} --node-ip =https://192.168.56.111" sh -
 #curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=$k3s_Token sh - --node-ip https://192.168.56.110
 
 # curl -sfL https://get.k3s.io | sh -s - 
