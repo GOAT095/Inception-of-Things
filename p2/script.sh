29802c8c7ade8cd6c21f26647fa0b4ae13cdc009
GOAT095 <hakimoxxx@gmail.com>
2024-03-21 17:21:32 +0000

    stupid copy past mistakes on script

--- p2/script.sh
+++ p2/script.sh
@@ -4,9 +4,9 @@ curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip 192.168.42.110
 sleep 30
 
 #Apply Kubernetes manifests
-kubectl apply -f apps/app1.yaml
-kubectl apply -f apps/app2.yaml
-kubectl apply -f apps/app3.yaml
+kubectl apply -f vagrant/apps/app1.yaml
+kubectl apply -f vagrant/apps/app2.yaml
+kubectl apply -f vagrant/apps/app3.yaml
 
 #Install Nginx Ingress Controller (ingress is an 
 #API object that helps developers expose their applications and manage external 
@@ -15,4 +15,4 @@ kubectl apply -f apps/app3.yaml
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/baremetal/deploy.yaml
 
 #Apply Ingress resource
-kubectl apply -f apps/ingress.yaml
+kubectl apply -f vagrant/apps/ingress.yaml
