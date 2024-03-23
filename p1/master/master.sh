16f7a1eddf73cde7dad952b1353d66e3fb928bf8
GOAT095 <hakimoxxx@gmail.com>
2024-03-23 02:15:06 +0000

    p1 seems perfect now kubectl works fine on SW

--- p1/master/master.sh
+++ p1/master/master.sh
@@ -9,13 +9,16 @@ flags="--tls-san $masterIP --node-external-ip $masterIP"  #helps so k3s master l
 echo "[INFO] Install k3s on master-node"
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="$flags" K3S_NODE_NAME="anassifS" K3S_KUBECONFIG_MODE="644" sh - #K3S_KUBECONFIG_MODE for perm so its access rancher folder
 
-# echo "[INFO] Expose K3S_TOKEN from master-node for worker-node setup"
-# cp "$k3sTokenFile" /vagrant/resources/.generated/k3s.token
 
+#sleep so k3s starts properly and we find the files
+sleep 30
 #cp the vagrant token to vagrant folder
 sudo rm -rf /vagrant/token.env
+sudo rm -rf /vagrant/k3s.yaml
+
 sudo cat /var/lib/rancher/k3s/server/token >> /vagrant/token.env
-sudo cp /etc/rancher/k3s/k3s.yaml  /vagrant/k3s.yaml
+sudo sed -i 's/127.0.0.1/192.168.56.110/g' /etc/rancher/k3s/k3s.yaml
+sudo cat /etc/rancher/k3s/k3s.yaml >> /vagrant/k3s.yaml
 
 #i need to take a look at this next to sole kubectl problem on slave
-#sudo vi /var/lib/rancher/k3os/config.yaml
+#sudo vi /var/lib/rancher/k3s/config.yaml
