f40bd740f93af3c50c5bf7bd81edee4a648a4eda
anassif <ubuntu@ubuntu2004.linuxvmimages.local>
2024-05-06 10:32:01 -0400

    darbet-las9et

--- p1/master/master.sh
+++ p1/master/master.sh
@@ -4,10 +4,10 @@
 
 masterIP="192.168.56.110"
 # k3sTokenFile="/var/lib/rancher/k3s/server/node-token"
-flags="--tls-san $masterIP --node-external-ip $masterIP"  #helps so k3s master listens on its real IP and accepts requests by putting its IP inside the cert.
+flags="--tls-san $masterIP --node-external-ip $masterIP --node-ip $masterIP"  #helps so k3s master listens on its real IP and accepts requests by putting its IP inside the cert.
 
 echo "[INFO] Install k3s on master-node"
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEsdcC="$flags" K3S_NODE_NAME="anassifS" K3S_KUBECONFIG_MODE="644" sh - #K3S_KUBECONFIG_MODE for perm so its access rancher folder
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="$flags" K3S_NODE_NAME="anassifS" K3S_KUBECONFIG_MODE="644" sh - #K3S_KUBECONFIG_MODE for perm so its access rancher folder
 
 
 #sleep so k3s starts properly and we find the files
