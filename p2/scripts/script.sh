2aa7dfc534b176ec6a855da32c14639dd144962d
GOAT095 <hakimoxxx@gmail.com>
2024-07-08 14:06:56 +0100

    p1 p2 latest version last

--- p2/scripts/script.sh
+++ p2/scripts/script.sh
@@ -24,4 +24,24 @@ kubectl delete -A ValidatingWebhookConfiguration ingress-nginx-admission
 #Apply Ingress resource]
 tput setaf 2 ; echo "applying ingress..."
 kubectl apply -f /vagrant/confs/apps/ingress.yaml
-sleep 60
+
+echo "Waiting for ingress to become ready..."
+while true; do
+    READY=$(kubectl get ingress web-ingress -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+    if [[ -n "$READY" ]]; then
+        echo "Ingress is ready with IP: $READY"
+        break
+    fi
+    sleep 20
+done
+
+echo "Ingress is successfully created and ready!"
+
+#net tools for ifconfig command not available in 20.04
+wget http://archive.ubuntu.com/ubuntu/pool/main/n/net-tools/net-tools_1.60+git20161116.90da8a0-1ubuntu1_amd64.deb
+
+dpkg-deb -x net-tools_1.60+git20161116.90da8a0-1ubuntu1_amd64.deb net-tools
+
+sudo cp net-tools/sbin/ifconfig /usr/local/bin/
+
+#sleep 300
